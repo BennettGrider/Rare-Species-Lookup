ffe49e6112948312344583bbf566b78c71f13b42
Bennett Grider <bennett.grider@mnrinc.us>
2019-02-05 12:35:50 -0600

    Get from Location button is now working. Cleaned up a lot of JS, but still more cleaning and refactoring to do. Next, add in some error/edge case handling, and add species/county data.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
 
+
 // Select2 as a jQuery function, vall on any jQuery selector when initializing Select2
 $(document).ready(function() {
     $('.county-selection-dropdown').select2({
@@ -22,7 +23,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Getting user location
 var userLat = 0;
 var userLng = 0;
-var latlngPoint = {lat: 0, lng: 0}
+var latlngPoint = {lat: 0, lng: 0};
 // var locMarker = L.marker([0, 0]);
 map.locate({setView: true, maxZoom: 12, enableHighAccuracy: true});
 function onLocationFound(e) {
@@ -38,15 +39,12 @@ map.on('locationfound', onLocationFound);
 
 // If geolocation fails
 map.on('locationerror', function onLocationError(e){
-    $('#get-from-map-col').append('<p id="loc-error">Location cannot be found.</p>');
+    $('#get-from-map-col').append('<p id="loc-error">Location cannot be found. Select a county on the map or dropdown to generate results.</p>');
 });
-
-
 
 
 // When the GeoJSON is loaded, determines what happens when a layer is clicked
 var clickedCounty = '';
-// var coords = [];
 function onEachFeature(feature, layer) {
     layer.bindPopup(feature.properties.CTY_NAME + ' County');
 
@@ -66,45 +64,51 @@ function onEachFeature(feature, layer) {
     layer.on({
         click: function(e) { // This function is used to store the clicked county in the clickedCounty variable
             clickedCounty = e.target.feature.properties.CTY_NAME;
+            // Also needs to set the Select2 dropdown to the clicked county name
+            $('.county-selection-dropdown').val(clickedCounty).trigger('change');
         }
     });
 }
 
-// Adding the counties GeoJSON and such. Simplified a lot by using leaflet-ajax
-var countiesLayer = new L.GeoJSON.AJAX('data/mn_counties_500.geojson', {
-    style: {
-        color: "#F5860F",
-        weight: 2,
-        opacity: 0.6,
-        fillOpacity: 0
-    },
-    onEachFeature: onEachFeature
-});
-// countiesLayer.addTo(map);
-countiesLayer.forEach(function(county) {
-    var polygon = L.polygon(state.geometry.coordinates, {
-        weight: 2,
-        fillOpacity: 0,
-        opacity: 0.8,
-        color: '#F5860F'
-    }).addTo(map);
+// Testing out adding geojson normally, without AJAX
+// This is done so that leaflet-pip can correctly interpret the object, and thus run pointInLayer properly
+var countiesLayer;
+$.getJSON('data/mn_counties_500.geojson', function(data){
+    countiesLayer = L.geoJSON(data, {
+        style: {
+            color: "#F5860F",
+            weight: 2.5,
+            opacity: 0.6,
+            fillOpacity: 0
+        },
+        onEachFeature: onEachFeature
+    });
+    countiesLayer.addTo(map);
 });
 
 
-
+// For adding the counties GeoJSON data to the map as polygons. Possibly useful, but not currently needed
+// countiesLayer.forEach(function(county) {
+//     var polygon = L.polygon(state.geometry.coordinates, {
+//         weight: 2,
+//         fillOpacity: 0,
+//         opacity: 0.8,
+//         color: '#F5860F'
+//     }).addTo(map);
+// });
 
 
 
 
 // Rare species found in each county. Formatted as a collection (array of objects)
 var all_counties = {
-    'Aitkin': ['Panax quinquefolius', 
+    Aitkin: ['Panax quinquefolius', 
                 'Littorella americana', 
                 'Botrychium oneidense', 
                 'Poa paludigena', 
                 'Juglans cinerea'],
-    'Anoka': ['aaa'],
-    'Ramsey': ['Aaaa', 'BBbBb']
+    Anoka: ['aaa'],
+    Ramsey: ['Aaaa', 'BBbBb']
 };
 
 
@@ -147,8 +151,6 @@ $('#get-rares-button').on('click', function (e) {
 });
 
 
-
-
 // On clicking 'Get from Location' button, does the same thing. Separate function because it handles getting data slightly differently
 $('#get-from-map-button').on('click', function (e) {
     if ($('.rare-title').length) {
@@ -156,35 +158,42 @@ $('#get-from-map-button').on('click', function (e) {
         $('#rare-list').empty(); // Use .empty() to also remove all child <li> elements
     };
 
-    // // Simulating a mouse click
-    // // var latlngPoint = locMarker.getLatLng();
-    // var latlngPoint = L.latLng(userLat, userLng);
-    // console.log(latlngPoint);
-    // // var id = parseInt(40);
-    // // var layer = countiesLayer.getLayer(id);
-    // map.fireEvent('click', {
-    //     latLng: latlngPoint,
-    //     layerPoint: map.latLngToLayerPoint(latlngPoint),
-    //     containerPoint: map.latLngToContainerPoint(latlngPoint)
-    // });
-    // var latlngPoint = L.latLng(userLat, userLng);
-    // console.log(latlngPoint);
-    // var locationLayer = map.getLayerAtLatLng(latlngPoint);
-    // var locLayer = this.layerAt(latLngToContainerPoint(latlngPoint));
-    // console.log(locationLayer);
-    // console.log(clickedCounty);
     $('.result-cols').show();
-    var selected_county = clickedCounty;
-    var sp_list_title = $('<h5 class="rare-title"></h5>').text(selected_county + ' County rare species:');
-    $('#county-species').prepend(sp_list_title);
-    $('#county-species').append('<ul id="rare-list"></ul>');
+    var selected_county = '';
 
-    // var county_species = all_counties[selected_county];
-    // for (var i = 0; i < county_species.length; i++) {
-    //     var sp_to_append = county_species[i];
-    //     var sp_common_name = sci_name_lookup[sp_to_append];
-    //     $('#rare-list').append('<li>' + sp_to_append + ' (' + sp_common_name + ')' + '</li>');
-    // };
+    // appendRareData function is used to ensure that none of this executes until the geolocation getCurrentPosition callback
+    function appendRareData(selected_county, all_counties, sci_name_lookup) {
+        var sp_list_title = $('<h5 class="rare-title"></h5>').text(selected_county + ' County rare species:');
+        $('#county-species').prepend(sp_list_title);
+        $('#county-species').append('<ul id="rare-list"></ul>');
+
+        var county_species = all_counties[selected_county];
+        console.log(selected_county);
+        console.log(all_counties[selected_county]);
+        for (var i = 0; i < county_species.length; i++) {
+            var sp_to_append = county_species[i];
+            var sp_common_name = sci_name_lookup[sp_to_append];
+            $('#rare-list').append('<li>' + sp_to_append + ' (' + sp_common_name + ')' + '</li>');
+        };
+    };
+    
+    // Checking to see if geolocation is available. If so, pressing the 'Get from Location' button grabs the county the user is located in using leaflet-pip
+    function geo_found(position) { 
+        // Using leaflet-pip to get the county at the position
+        var countyResult = leafletPip.pointInLayer([userLng, userLat], countiesLayer);
+        var countyGeoloc = countyResult[0].feature.properties.CTY_NAME;
+        selected_county = countyGeoloc;
+        // console.log('Found position');
+        appendRareData(selected_county, all_counties, sci_name_lookup);
+    }
+    function geo_error() {
+        selected_county = clickedCounty;
+        // console.log('Cant find position');
+        appendRareData(selected_county, all_counties, sci_name_lookup);
+    }
+    
+    navigator.geolocation.getCurrentPosition(geo_found, geo_error);
+    
 });
 
 
